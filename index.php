fba34cce9c382449c57cb6e3153a03318e3682e5
dongkkase <eyecandyzero@naver.com>
2023-05-10 15:32:57 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require_once("SeedECB.class.php");
 
-$seed = new SeedECB('1234567812345678');
+$seed = new SeedECB('1234567812345678'); // Key
 
 $enc = $seed->encrypt('test');
-echo $enc;
+echo $enc; // k1K+z79CYi45WGuevLq+cA==
 
 $dec = $seed->decrypt($enc);
-echo $dec;
+echo $dec; // test
